64e1cdb5a0803a091221da5c9868a49e352dbca2
Connor Kormos <connorkormos97@gmail.com>
2023-02-15 20:50:01 -0800

    JavaScript functions are a work in progress.  Basic functions should be mostly set, but the parameters for their ability to function need work.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # my-maze-v2
+
+The goal of this maze is to utilize both tables in my HTML, CSS styling, and JavaScript funcitons to create a maze (not auto generated, at least not yet).  The user should be able to use arrow keys as input to control the player character's movement, within dictated boundaries, to move from the starting point to the finish line.
--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const array = [
  12[0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
  13[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
  14[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//     1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21
+//  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23
 ];
 
+/*
 const table = document.getElementById("my-table");
-
 
 window.onload = function() {
 for (let i = 0; i < array.length; i++) {
@@ -33,12 +33,16 @@ for (let i = 0; i < array.length; i++) {
   table.appendChild(row);
 }
 }
+*/
 /*
-let wallVal = document.get
+var person = null;
+var row = null;
+
+let wallVal = document.querySelector("td[data-value]").value;
 
 function init() {
   person = document.getElementById("person");
-  person.style.position = "absolute";
+  person.style.position = "relative";
   person.style.left = "0";
   person.style.top = "0";
 }
@@ -47,11 +51,77 @@ function movePersonRight() {
   const rightNumbers = person.style.left.replace("px", "");
   const left = parseInt(rightNumbers, 10);
 
-  if () {
+  if (wallVal === 0) {
       person.style.left = `${left + 10}px`;
   }
 }
+*/
 
+var person = null;
+var row = null;
+
+let wallVal = document.querySelector("mazeVal").value;
+
+function init() {
+    person = document.getElementById("person");
+    person.style.position = "absolute";
+    person.style.left = "0";
+    person.style.top = "0";
+}
+
+function movePersonRight() {
+    const rightNumbers = person.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (element.css('background-color') == "#FFF") {
+        person.style.left = `${left + 10}px`;
+    }
+}
+
+function movePersonLeft() {
+    const leftNumbers = person.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (element.css('background-color') == "#FFF") {
+        person.style.left = `${left - 10}px`;
+    }
+}
+
+function movePersonUp() {
+    const topNumbers = person.style.top.replace("px", "");
+    const top = parseInt(topNumbers, 10);
+
+    if (element.css('background-color') == "#FFF") {
+        person.style.top = `${top - 10}px`;
+    }
+}
+
+function movePersonDown() {
+  const bottomNumbers = person.style.top.replace("px", "");
+  const top = parseInt(bottomNumbers, 10);
+
+  if (element.css('background-color') == "#FFF") {
+      person.style.top = `${top - 10}px`;
+  }
+}
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+      movePersonLeft();
+  }
+  if (e.key === "ArrowRight") {
+      movePersonRight();
+  }
+  if (e.key === "ArrowUp") {
+      movePersonUp();
+  }
+  if (e.key === "ArrowDown") {
+      movePersonDown();
+  }
+});
+
+window.onload = init;
 
 /*
 // Set the starting and ending points
